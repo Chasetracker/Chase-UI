a21d513f52427b66ddc240b9a2fcb3891fde6ef0
ajalapraise <akintomiisin@gmail.com>
2023-11-25 15:20:23 +0100

    Add DashboardLayout component and react-toastify dependency

---
+++ src/pages/dashboard/index.tsx
@@ -0,0 +1,15 @@
+import DashboardLayout from '@/components/Layout/DashboardLayout'
+import React from 'react'
+
+
+const index = () => {
+    return (
+        <div>
+            <DashboardLayout />
+            <div></div>
+
+        </div>
+    )
+}
+
+export default index
