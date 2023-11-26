a4cade6077b451fcc0e21d2f124e881789ca5dd7
ajalapraise <akintomiisin@gmail.com>
2023-11-26 18:53:05 +0100

    Update dashboard layout and add transaction section

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -122,18 +122,13 @@ const Home: React.FC<FormProps> = () => {
             <DashboardLayout>
                 <ToastContainer />
                 <main className='relative w-full h-full '>
-                    <div className='opacity-5'>
-                        <h1 className='text-lg font-extrabold'>Welcome back, GroceryHub</h1>
-                        <h2 className='text-[16px] font-light'>Track, manage and forecast your customers and orders.</h2>
-                    </div>
-
-                    <div className={`absolute flex flex-col justify-center items-center w-[400px] top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 h-[400px] border-[1px] border-[red] rounded-md bg-white p-3 ${isOpened ? '' : 'hidden'}`}>
+                    <div className={`absolute flex flex-col justify-center items-center w-[400px] top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 h-[400px] border-[0.5px]  border-[#667085] rounded-md bg-[#FFF] shadow-md p-3 ${isOpened ? '' : 'hidden'}`}>
                         <div className='flex justify-between items-start w-full mb-3'>
                             <div>
                                 <h1 className='text-[20px] font-extrabold text-black'>Add your Account Details</h1>
                                 <h2 className='text-base text-[#667085]'>Complete your account creation</h2>
                             </div>
-                            <div className='text-[20px] flex justify-center items-center mt-3 hover:text-[21px] cursor-pointer'
+                            <div className='text-[20px] flex justify-center items-center mt-3 hover:text-[21px] cursor-pointer text-[#667085]'
                                 onClick={closeModal}>
                                 <ImCancelCircle />
                             </div>
@@ -205,7 +200,7 @@ const Home: React.FC<FormProps> = () => {
                                     className={`w-3/4 bg-white text-[#314155] py-2 px-4 border-[1px] border-[#CCD5DF] rounded-md text-sm hover:bg-[red] hover:text-white`}
                                     onClick={closeModal}
                                 >
-                                    cancel
+                                    Cancel
                                 </button>
                                 <button
                                     type="submit"
@@ -218,6 +213,38 @@ const Home: React.FC<FormProps> = () => {
                             </div>
                         </form>
                     </div>
+                    <div className={`${isOpened ? 'opacity-[0.3]' : ''} flex flex-col`}>
+                        <h1 className='text-lg font-extrabold'>Welcome back, GroceryHub</h1>
+                        <h2 className='text-[16px] font-light'>Track, manage and forecast your customers and orders.</h2>
+                    </div>
+
+                    <section>
+                        <div className='w-full h-[70px] flex justify-between items-center'>
+                            <h1 className='text-lg font-extrabold text-black'>Transactions</h1>
+                            <div className='w-[225px] flex justify-between items-center'>
+                                <button
+                                    type="button"
+                                    className={`w-[75px] bg-white text-[#314155] border-[1px] border-[#CCD5DF] p-2 text-[12px] rounded-l-md hover:bg-[#F8FAFB]`}
+                                >
+                                    Today
+                                </button>
+                                <button
+                                    type="button"
+                                    className={`w-[75px] bg-white text-[#314155]  border-t-[1px] border-b-[1px] border-[#CCD5DF] p-2 text-[12px] hover:bg-[#F8FAFB]`}
+                                >
+                                    This Week
+                                </button>
+                                <button
+                                    type="button"
+                                    className={`w-[75px] bg-white text-[#314155] p-2 text-[12px]  border-[1px] border-[#CCD5DF] rounded-r-md hover:bg-[#F8FAFB]  `}
+                                >
+                                    This Month
+                                </button>
+                            </div>
+                        </div>
+                    </section>
+
+
                 </main>
             </DashboardLayout>
         </>
